70e8e3a732828ff704a7af6d6bf75916c0675714
Mikel Bishop <mdrew8@gmail.com>
2020-08-29 18:40:10 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,5 +3,5 @@ def prime?(number)
 return false if number.integer? == false
 return false if number < 2 
 return true if number == 2 
-(2..number-1) 
+(2..number-1).each {|num| return false if number % num == 0}
 end
